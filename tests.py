4675af7f95a71686cd67759fa63c860b81110f6c
Jan Ruhfus <janruhfus0@gmail.com>
2022-01-19 14:12:45 +0100

    Add unit test

---
+++ tests.py
@@ -0,0 +1,20 @@
+"""
+This class is used to test the unit tests.
+"""
+
+import unittest
+
+class TestUnit(unittest.TestCase):
+    """
+    Sample unit test group
+    """
+
+    def test_unit(self):
+        """
+        Sample unit test
+        """
+
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()

06b6cbe75fa5bf29c307b7f17a12bf8eb8633fc2
Jan Ruhfus <janruhfus0@gmail.com>
2022-01-25 15:10:30 +0100

    Create wrapper for joystick

--- src/wrapper/joystick.py
+++ src/wrapper/joystick.py
@@ -1,3 +1,5 @@
+import RPi.GPIO as GPIO
+
 class Keymap:
     """BCM keymap for joystick buttons"""
     UP = 6
@@ -5,3 +7,34 @@ class Keymap:
     LEFT = 5
     RIGHT = 26
     PRESS = 13
+
+class Joystick:
+    def __init__(self, up, down, left, right, click):
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
+        self.click = click
+
+        GPIO.setup(GPIO.BCM)
+
+        GPIO.setup(self.up,    GPIO.IN,    pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.down,  GPIO.IN,    pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.left,  GPIO.IN,    pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.right, GPIO.IN,    pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.click, GPIO.IN,    pull_up_down=GPIO.PUD_UP)
+
+    def is_pressed_up(self):
+        return GPIO.input(self.up)
+
+    def is_pressed_down(self):
+        return GPIO.input(self.down)
+
+    def is_pressed_left(self):
+        return GPIO.input(self.left)
+
+    def is_pressed_right(self):
+        return GPIO.input(self.right)
+
+    def is_pressed_click(self):
+        return GPIO.input(self.click)

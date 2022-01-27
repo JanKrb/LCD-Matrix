42aa0da260fe2046ee369774f168018345fed607
Jan Ruhfus <janruhfus0@gmail.com>
2022-01-27 14:19:35 +0100

    Add button events

--- src/wrapper/buttons.py
+++ src/wrapper/buttons.py
@@ -14,11 +14,27 @@ class Buttons:
         self.key2 = key2
         self.key3 = key3
 
+        self.event_key1 = None
+        self.event_key2 = None
+        self.event_key3 = None
+
+        self._setup_keys()
+        self._setup_events()
+
+    def _setup_keys(self):
         GPIO.setup(GPIO.BCM)
 
-        GPIO.setup(key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def _setup_events(self):
+        if self.event_key1 is not None:
+            GPIO.add_event_detect(self.key1, GPIO.RISING, callback=self.event_key1)
+        if self.event_key2 is not None:
+            GPIO.add_event_detect(self.key1, GPIO.RISING, callback=self.event_key2)
+        if self.event_key3 is not None:
+            GPIO.add_event_detect(self.key1, GPIO.RISING, callback=self.event_key3)
 
     def is_pressed_key1(self):
         return GPIO.input(self.key1)

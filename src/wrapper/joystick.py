c925d23f25740fa73c03d7e296aa31dd5b341b79
Jan Ruhfus <janruhfus0@gmail.com>
2022-01-27 14:22:48 +0100

    Add joystick events

--- src/wrapper/joystick.py
+++ src/wrapper/joystick.py
@@ -18,6 +18,16 @@ class Joystick:
         self.right = right
         self.click = click
 
+        self.event_up = None
+        self.event_down = None
+        self.event_left = None
+        self.event_right = None
+        self.event_click = None
+
+        self._setup_keys()
+        self._setup_events()
+
+    def _setup_keys(self):
         GPIO.setup(GPIO.BCM)
 
         GPIO.setup(self.up, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -25,6 +35,18 @@ class Joystick:
         GPIO.setup(self.left, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.right, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.click, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def _setup_events(self):
+        if self.event_up is not None:
+            GPIO.add_event_detect(self.up, GPIO.RISING, callback=self.event_up)
+        if self.event_down is not None:
+            GPIO.add_event_detect(self.down, GPIO.RISING, callback=self.event_down)
+        if self.event_left is not None:
+            GPIO.add_event_detect(self.left, GPIO.RISING, callback=self.event_left)
+        if self.event_right is not None:
+            GPIO.add_event_detect(self.right, GPIO.RISING, callback=self.event_right)
+        if self.event_click is not None:
+            GPIO.add_event_detect(self.click, GPIO.RISING, callback=self.event_click)
 
     def is_pressed_up(self):
         return GPIO.input(self.up)

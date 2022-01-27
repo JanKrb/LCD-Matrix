8f13a9ee99de1dec0531d7dbd2f3476bc46a8298
Jan Ruhfus <janruhfus0@gmail.com>
2022-01-27 14:00:18 +0100

    Refactor

--- src/wrapper/buttons.py
+++ src/wrapper/buttons.py
@@ -1,10 +1,12 @@
 import RPi.GPIO as GPIO
 
+
 class Keymap:
     """BCM keymap for left-sided buttons"""
     KEY1 = 21
     KEY2 = 20
     KEY3 = 16
+
 
 class Buttons:
     def __init__(self, key1, key2, key3):
@@ -13,16 +15,16 @@ class Buttons:
         self.key3 = key3
 
         GPIO.setup(GPIO.BCM)
-        
-        GPIO.setup(key1,    GPIO.IN,    pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(key2,    GPIO.IN,    pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(key3,    GPIO.IN,    pull_up_down=GPIO.PUD_UP)
-    
+
+        GPIO.setup(key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
     def is_pressed_key1(self):
         return GPIO.input(self.key1)
-    
+
     def is_pressed_key2(self):
         return GPIO.input(self.key2)
-    
+
     def is_pressed_key3(self):
         return GPIO.input(self.key3)
--- src/wrapper/joystick.py
+++ src/wrapper/joystick.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 
+
 class Keymap:
     """BCM keymap for joystick buttons"""
     UP = 6
@@ -7,6 +8,7 @@ class Keymap:
     LEFT = 5
     RIGHT = 26
     PRESS = 13
+
 
 class Joystick:
     def __init__(self, up, down, left, right, click):
@@ -18,11 +20,11 @@ class Joystick:
 
         GPIO.setup(GPIO.BCM)
 
-        GPIO.setup(self.up,    GPIO.IN,    pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.down,  GPIO.IN,    pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.left,  GPIO.IN,    pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.right, GPIO.IN,    pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.click, GPIO.IN,    pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.up, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.down, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.left, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.right, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.click, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def is_pressed_up(self):
         return GPIO.input(self.up)

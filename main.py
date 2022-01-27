6047a46d670570925ed0c7e9eeb807aeef62004f
Jan Ruhfus <janruhfus0@gmail.com>
2022-01-27 13:04:25 +0100

    Add sample programm

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+from src.wrapper.sh1106 import SH1106
+from PIL import Image,ImageDraw,ImageFont
+import time
+import os
+
+font_path = os.path.join('assets', 'Font.ttf')
+
+def main():
+    disp = SH1106()
+
+    disp.Init()
+    disp.clear()
+
+    start_image = Image.new('1', (disp.width, disp.height), "WHITE")
+    draw = ImageDraw.Draw(start_image)
+    font = ImageFont.truetype(font_path, 20)
+
+    draw.line([(0,0),(127,0)], fill = 0)
+    draw.line([(0,0),(0,63)], fill = 0)
+    draw.line([(0,63),(127,63)], fill = 0)
+    draw.line([(127,0),(127,63)], fill = 0)
+
+    draw.text((30,0), 'Niclas Heide', font = font, fill = 0)
+    draw.text((28,20), 'Jan Ruhfus ', font = font, fill = 0)
+
+    disp.ShowImage(disp.getbuffer(start_image))
+
 if __name__ == '__main__':
-    pass
+    main()

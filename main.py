4c8f84aee326d3b5ed4e0d4984b0c9ea1d6645d4
Jan Ruhfus <janruhfus0@gmail.com>
2022-02-04 11:41:05 +0100

    Refactor StartMenu into seperate menu class

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
-from src.wrapper.sh1106 import SH1106, Screen, SPI
-from PIL import Image, ImageDraw, ImageFont
+from src.wrapper.sh1106 import SH1106, SPI
+from src.menu.start_menu import StartMenu
 import RPi.GPIO as GPIO
-import os
-
-font_path = os.path.join('assets', 'Font.ttf')
-
 
 def main():
     disp = SH1106()
 
     disp.Init()
     disp.clear()
-
-    start_image = Image.new('1', (disp.width, disp.height), "WHITE")
-    draw = ImageDraw.Draw(start_image)
-    font = ImageFont.truetype(font_path, 20)
-
-    draw.line([(0, 0), (Screen.width - 1, 0)], fill=0)  # Top
-    draw.line([(0, 0), (0, Screen.height - 1)], fill=0)  # Left
-    draw.line([(0, Screen.height - 1), (Screen.width - 1, Screen.height - 1)], fill=0)  # Bottom
-    draw.line([(Screen.width - 1, 0), (Screen.width - 1, Screen.height - 1)], fill=0)  # Right
-
-    draw.text((5, 5), 'Niclas Heide', font=font, fill=0)
-    draw.text((5, 25), 'Jan Ruhfus ', font=font, fill=0)
-
-    disp.show_image(disp.get_buffer(start_image))
+    
+    start_screen = StartMenu()
+    disp.show_image(disp.get_buffer(start_screen.start_image))
 
     try:
         while True: pass

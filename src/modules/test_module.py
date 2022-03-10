920c7a6f9a91e9b5f2799d3a7058b7fc4f7f0301
Jan Ruhfus <janruhfus0@gmail.com>
2022-03-10 10:32:21 +0100

    test delay

--- src/modules/test_module.py
+++ src/modules/test_module.py
@@ -1,10 +1,9 @@
 from src.modules.module import Module as TemplateModule
+import time
 
 class Module(TemplateModule):
     def __init__(self, display):
         super().__init__(display)
-
-
     
     def draw(self):
         super().draw()
@@ -12,3 +11,4 @@ class Module(TemplateModule):
     def update(self):
         super().update()
         print("test")
+        time.sleep(5)

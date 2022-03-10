78938e4639cf8d1d1ac5b440f8b1677f21268c3f
Jan Ruhfus <janruhfus0@gmail.com>
2022-03-10 09:55:34 +0100

    create test output for test module

--- src/modules/test_module.py
+++ src/modules/test_module.py
@@ -3,3 +3,10 @@ from src.modules.module import Module as TemplateModule
 class Module(TemplateModule):
     def __init__(self):
         super().__init__()
+    
+    def draw(self):
+        super().draw()
+
+    def update(self):
+        super().update()
+        print("test")

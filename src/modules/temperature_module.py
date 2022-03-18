5baacb0bff2ee8fedd40781be15a6ca641296274
Niclas Heide <niclas.heide@students.tbs1.de>
2022-03-18 11:14:55 +0100

    Temperature on display

--- src/modules/temperature_module.py
+++ src/modules/temperature_module.py
@@ -11,9 +11,6 @@ class Module(TemplateModule):
         
         self.display = display
         
-        self.txt_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"sensor_data.txt")
-        
-        self.temperature = "0C"
         self.font = ImageFont.truetype(font_path, 20)
         
         self.start_image = Image.new('1', (display.width, display.height), "WHITE")
@@ -23,16 +20,16 @@ class Module(TemplateModule):
     def get_data(self):
         f = open("sensor_data.txt","r")
         data = json.loads(f.read())
-        print(data)
-        #with open(self.txt_path,"r") as f:
-            #return f.read()
+        return data["temperature"], data["humidity"]
      
     def draw(self):
+        temperature, humidity = self.get_data()
         self.start_image = Image.new('1', (self.display.width, self.display.height), "WHITE")
         self.draw_object = ImageDraw.Draw(self.start_image)
         self.display.clear()
 
-        self.draw_object.text((5, 5), self.temperature, font=self.font, fill=0)
+        self.draw_object.text((5, 5), temperature, font=self.font, fill=0)
+        self.draw_object.text((5, 30), humidity, font=self.font, fill=0)
 
         return self.start_image
     

37ce0214c3ccc9fd661f0be4a6125d1effee81c1
Jan Ruhfus <janruhfus0@gmail.com>
2022-03-18 11:56:22 +0100

    try fix crash

--- src/modules/clock_module.py
+++ src/modules/clock_module.py
@@ -12,7 +12,7 @@ class Module(TemplateModule):
         self.time = "00:00:00"
         self.font = ImageFont.truetype(font_path, 20)
 
-        self.start_image = Image.new(self.time, (display.width, display.height), "WHITE")
+        self.start_image = Image.new('1', (display.width, display.height), "WHITE")
         self.draw = ImageDraw.Draw(self.start_image)
 
     def draw(self):

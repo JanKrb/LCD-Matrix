7b2d5c9299efc805d9ebaa4b8b81b56b4e607410
Jan Ruhfus <janruhfus0@gmail.com>
2022-03-18 12:35:40 +0100

    fix random crash

--- src/modules/temperature_module.py
+++ src/modules/temperature_module.py
@@ -27,18 +27,21 @@ class Module(TemplateModule):
         return data["temperature"], data["humidity"]
      
     def draw(self):
-        temperature, humidity = self.get_data()
+        try:
+            temperature, humidity = self.get_data()
 
-        if temperature != self.temperature or humidity != self.humidity:
-            self.temperature = temperature
-            self.humidity = humidity
-        
-            self.start_image = Image.new('1', (self.display.width, self.display.height), "WHITE")
-            self.draw_object = ImageDraw.Draw(self.start_image)
-            self.display.clear()
-            self.draw_object.text((5, 5), f"Temp: {temperature} °C", font=self.font, fill=0)
-            self.draw_object.text((5, 30), f"Hum: {humidity} %", font=self.font, fill=0)
-
+            if temperature != self.temperature or humidity != self.humidity:
+                self.temperature = temperature
+                self.humidity = humidity
+            
+                self.start_image = Image.new('1', (self.display.width, self.display.height), "WHITE")
+                self.draw_object = ImageDraw.Draw(self.start_image)
+                self.display.clear()
+                self.draw_object.text((5, 5), f"Temp: {temperature} °C", font=self.font, fill=0)
+                self.draw_object.text((5, 30), f"Hum: {humidity} %", font=self.font, fill=0)
+        except:
+            pass
+    
         return self.start_image
     
     def update(self):

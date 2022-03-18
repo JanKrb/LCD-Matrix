88cf08032eb39d13bc88346984c9523dd461b138
Niclas Heide <niclas.heide@students.tbs1.de>
2022-03-18 11:03:07 +0100

    temperature test

--- src/modules/temperature_module.py
+++ src/modules/temperature_module.py
@@ -1,5 +1,22 @@
 from src.modules.module import Module as TemplateModule
+import os
+import json
 
 class Module(TemplateModule):
-    def __init__(self, display):
-        super().__init__(display)
+    def __init__(self):
+        super().__init__()
+        self.txt_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"sensor_data.txt")
+    
+    def get_data(self):
+        f = open(self.txt_path,"r")
+        data = json.loads(f.read())
+        print(data)
+        #with open(self.txt_path,"r") as f:
+            #return f.read()
+     
+    def draw(self):
+        pass
+    
+    def update(self):
+        self.get_data()
+    
